b351d973b23360d85ea534293fb2072c88f60337
Charles Kasasira <charleskasasira01@gmail.com>
2022-03-05 18:57:31 +0300

    course unit cards

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, Image, ScrollView } from 'react-native'
 import React from 'react'
 
 const HomeScreen = () => {
@@ -31,6 +31,115 @@ const HomeScreen = () => {
             </View>
             
         </View>
+
+        <View 
+            style={{flexDirection: 'row', justifyContent: 'space-between',
+            marginHorizontal: 20, marginTop: 10
+        }}
+        >
+            <Text style={{fontSize: 20}}>Upcoming Events</Text>
+            <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>See all</Text>
+        </View>
+
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <View
+                style={{
+                    flex: 1,
+                    height: 200,
+                    width: 200,
+                    borderRadius: 20,
+                    backgroundColor: '#fff',
+                    elevation: 10,
+                    margin: 20,
+                    overflow: 'hidden'
+                }}
+            >
+                <View style={{flex: 0.7, backgroundColor: '#000', justifyContent: 'center', alignItems: 'center', paddingHorizontal: 10}}>
+                    <Text style={{color: '#fff', fontWeight: 'bold', fontSize: 18}}>Digital Innovation and Computational Thinking</Text>
+                </View>
+                <View style={{flex: 0.3}}>
+                    <Text>Nothing yet</Text>
+                </View>
+            </View>
+            <View
+                style={{
+                    height: 200,
+                    width: 200,
+                    borderRadius: 20,
+                    backgroundColor: '#fff',
+                    elevation: 10,
+                    margin: 20,
+                    overflow: 'hidden'
+                }}
+            >
+                <View style={{flex: 0.7, backgroundColor: 'pink', justifyContent: 'center', alignItems: 'center', paddingHorizontal: 10}}>
+                    <Text style={{color: '#fff', fontWeight: 'bold', fontSize: 18}}>Computer Literacy</Text>
+                </View>
+                <View style={{flex: 0.3}}>
+                    <Text>Nothing yet</Text>
+                </View>
+            </View>
+            <View
+                style={{
+                    height: 200,
+                    width: 200,
+                    borderRadius: 20,
+                    backgroundColor: '#fff',
+                    elevation: 10,
+                    margin: 20,
+                    overflow: 'hidden'
+                }}
+            >
+                <View style={{flex: 0.7, backgroundColor: 'crimson', justifyContent: 'center', alignItems: 'center', paddingHorizontal: 10}}>
+                <Text style={{color: '#fff', fontWeight: 'bold', fontSize: 18}}>Computer Organization and Architecture</Text>
+                </View>
+                <View style={{flex: 0.3}}>
+                    <Text>Nothing yet</Text>
+                </View>
+            </View>
+
+            <View
+                style={{
+                    height: 200,
+                    width: 200,
+                    borderRadius: 20,
+                    backgroundColor: '#fff',
+                    elevation: 10,
+                    margin: 20,
+                    overflow: 'hidden'
+                }}
+            >
+                <View style={{flex: 0.7, backgroundColor: 'grey', justifyContent: 'center', alignItems: 'center', paddingHorizontal: 10}}>
+                <Text style={{color: '#fff', fontWeight: 'bold', fontSize: 18}}>Structured and Object Oriented Programming</Text>
+                </View>
+                <View style={{flex: 0.3}}>
+                    <Text>Nothing yet</Text>
+                </View>
+            </View>
+
+
+            <View
+                style={{
+                    height: 200,
+                    width: 200,
+                    borderRadius: 20,
+                    backgroundColor: '#fff',
+                    elevation: 10,
+                    margin: 20,
+                    overflow: 'hidden'
+                }}
+            >
+                <View style={{flex: 0.7, backgroundColor: 'blue', justifyContent: 'center', alignItems: 'center', paddingHorizontal: 10}}>
+                <Text style={{color: '#fff', fontWeight: 'bold', fontSize: 18}}>Communication Skills</Text>
+                </View>
+                <View style={{flex: 0.3}}>
+                    <Text>Nothing yet</Text>
+                </View>
+            </View>
+        </ScrollView>
+
+
+        
     </SafeAreaView>
   )
 }
